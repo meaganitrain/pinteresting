5894f9010949a277428458ced741aded38d953ad
Meagan Cook <macook@utexas.edu>
2015-10-20 22:40:02 -0500

    Changed S3 Bucket Name to secret-tunnels

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -16,12 +16,12 @@ class PinsController < ApplicationController
   def edit
   end
 
-  def create
+ def create
     @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render action: 'new'
+      render :new
     end
   end
 
